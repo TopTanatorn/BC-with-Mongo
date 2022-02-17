6464a6a8af6f27a385f01a13c7b6ab90a195a4e7
TopTanatorn <s6230613017@phuket.psu.ac.th>
2022-02-17 23:18:10 +0700

    pototype

---
+++ src/main.js
@@ -0,0 +1,88 @@
+const mqtt = require("mqtt");
+const { Blockchain, Block } = require('./blockchain');
+const MQTT_SERVER = "192.168.0.164";
+const MQTT_PORT = "1883";
+const roomTemp = "room1/temp";
+const roomLdr = "room1/ldr";
+const roomLed1 = "room1/led1";
+const roomLed2 = "room1/led2";
+var i=0,j=0,k=0,l=0;
+
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/";//mogodb client
+let iotChainTemp = new Blockchain();//create chain
+let iotChainLDR = new Blockchain();//create chain
+let iotChainLED1 = new Blockchain();//create chain
+let iotChainLED2 = new Blockchain();//create chain
+
+iotChainTemp.addBlock(iotChainTemp.createGenesisBlock());
+iotChainLDR.addBlock(iotChainLDR.createGenesisBlock());
+iotChainLED1.addBlock(iotChainLED1.createGenesisBlock());
+iotChainLED2.addBlock(iotChainLED2.createGenesisBlock());
+// Connect MQTT
+var client = mqtt.connect({
+    host: MQTT_SERVER,
+    port: MQTT_PORT,
+
+});
+
+client.on('connect', function () {
+    // Subscribe any topic
+    console.log("MQTT Connect");
+    client.subscribe("#", function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+});
+
+// Receive Message and print on terminal
+
+MongoClient.connect(url, function (err, db) {//connect database mongo db
+    if (err) throw err;
+    client.on('message', function (topic, message) {//connect MQTT
+        // message is Buffer
+        if(topic === roomTemp ){
+            iotChainTemp.addBlock(new Block(Date.now(), message.toString()));//creare block chain
+            var cname = roomTemp;
+            chain = iotChainTemp.chain[i];//define the current block
+            i = i+1;
+        }
+        else if(topic === roomLdr ){
+            iotChainLDR.addBlock(new Block(Date.now(), message.toString()));//creare block chain
+            var cname = roomLdr;
+            chain = iotChainLDR.chain[j];//define the current block
+            j = j+1;
+        }
+        else if(topic === roomLed1 ){
+            iotChainLED1.addBlock(new Block(Date.now(), message.toString()));//creare block chain
+            var cname = roomLed1;
+            chain = iotChainLED1.chain[k];//define the current block
+            k = k+1;
+        }
+        else if(topic === roomLed2 ){
+            iotChainLED2.addBlock(new Block(Date.now(), message.toString()));//creare block chain
+            var cname = roomLed2;
+            chain = iotChainLED2.chain[l];//define the current block
+            l = l+1
+        }
+        var dbo = db.db("BC-Database");//add to colection name
+        console.log(topic.toString());
+        console.log(message.toString());
+
+        
+
+        var myobj = [
+            { chain },//insert chain on database
+
+        ];
+
+        
+        dbo.collection(cname).insertMany(myobj, function (err, res) {//add block to mongodb database
+            if (err) throw err;
+            
+
+        });
+    });
+
+});
